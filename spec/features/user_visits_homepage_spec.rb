8793bf9a02b266baf3fa5e2f3d35dd6dcf6a0285
Camila <trmcami@gmail.com>
2016-08-20 14:33:47 -0300

    Build initial layout for media listing

--- spec/features/user_visits_homepage_spec.rb
+++ spec/features/user_visits_homepage_spec.rb
@@ -4,6 +4,6 @@ feature 'User visits homepage' do
   scenario 'sees media list' do
     visit root_path
 
-    expect(page).to have_css 'ul.media-list'
+    expect(page).to have_css 'div.media-list'
   end
 end
