e1ec9722fbf3c9e193fbd7cfed10b23607bd68a4
Camila <trmcami@gmail.com>
2016-09-03 14:13:30 -0300

    Require missing support files

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -22,6 +22,9 @@ require 'rspec/rails'
 #
 # Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
+require 'support/database_cleaner'
+require 'support/clearance'
+
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
